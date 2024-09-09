5aa5198e173e9793923787ef7d330fc4e68b6b40
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2024-09-09 17:22:31 +0800

    Initial Commit

---
+++ SST Inc Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SST Inc Project
+//
+//  Created by Joseph Kevin Fredric on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SST Inc Project/SST_Inc_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SST_Inc_ProjectApp.swift
+//  SST Inc Project
+//
+//  Created by Joseph Kevin Fredric on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SST_Inc_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
