a1896607bec24a58b2861eed0f4904f5b5f9b046
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2024-09-13 19:42:33 +0800

    Made a progress bar

--- SST Inc Project/ContentView.swift
+++ SST Inc Project/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack{
+                ProgressBar()
+            }
         }
-        .padding()
     }
 }
 
---
+++ SST Inc Project/ProgressBar.swift
@@ -0,0 +1,39 @@
+//
+//  ProgressBar.swift
+//  SST Inc Project
+//
+//  Created by Joseph Kevin Fredric on 13/9/24.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    @State var progress: CGFloat = 0.7
+    @State var totalWidth: CGFloat = 300
+    @State var dashWidth: CGFloat = 8
+    @State var dashSpacing: CGFloat = 2
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5){
+            Text("\(Int(progress * 100)) %")
+                .bold()
+                .font(.title3)
+            ZStack(alignment: .leading){
+                Rectangle()
+                    .stroke(lineWidth: 2)
+                    .frame(width: totalWidth, height: 20)
+                HStack(spacing: dashSpacing){
+                    ForEach(0 ..< Int((progress * totalWidth) / (dashWidth + dashSpacing)), id: \.self) { item in
+                        Rectangle()
+                            .frame(width: dashWidth, height:15)
+                    }
+                }
+                .offset(x: 3)
+            }
+        }
+        .foregroundStyle(.green)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
